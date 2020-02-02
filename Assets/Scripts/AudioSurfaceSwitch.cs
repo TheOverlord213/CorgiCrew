7c30793dbac518f54d9a1c0e05272de4b4517bb9
LEE FAIRWEATHER <1402381@uad.ac.uk>
2020-02-02 09:36:00 +0000

    sound stuff

---
+++ Assets/Scripts/AudioSurfaceSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class AudioSurfaceSwitch : MonoBehaviour
+{
+    public string SwitchGroup = "SurfaceMaterial";
+    public string Switch = "Grass";
+    public string ExitSwitch = "Grass";
+    public GameObject player;
+
+
+    void Start()
+    {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Switch surface Wwise footsteps");
+            AkSoundEngine.SetSwitch(SwitchGroup, Switch, player);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AkSoundEngine.SetSwitch(SwitchGroup, ExitSwitch, player);
+        }
+    }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BARK = 1274655755U;
+        static const AkUniqueID DOG_FOOT = 698346356U;
+        static const AkUniqueID FABRIC_TEAR = 1784312585U;
+        static const AkUniqueID FOOT_PLAYER = 1894362767U;
+        static const AkUniqueID GROWL_DOG = 1055264203U;
+        static const AkUniqueID LEVEL_MUSIC = 1244594577U;
+        static const AkUniqueID METAL_FALL = 1270178218U;
+        static const AkUniqueID METAL2_FALL = 2564875616U;
+        static const AkUniqueID TUBS_FALL = 2914442879U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOT
+        {
+            static const AkUniqueID GROUP = 3031504797U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID TILE = 2637588553U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOT
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CORGISB = 1981939356U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
