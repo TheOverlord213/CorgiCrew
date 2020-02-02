511f4a2de58b129ff386fe815eee94e824eb662d
Sean Docherty <sean.docherty213@hotmail.com>
2020-02-02 10:37:45 +0000

    Finalised Audio

---
+++ Assets/Scripts/PostBarkEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostBarkEvent : MonoBehaviour
+{
+    public AK.Wwise.Event WwiseEvent;
+    // Start is called before the first frame update
+    public void PlayBarkSound()
+    {
+        AkSoundEngine.PostEvent("bark", gameObject);
+        WwiseEvent.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PostDogFootstep.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostDogFootstep : MonoBehaviour
+{
+    public AK.Wwise.Event WwiseEvent;
+    // Start is called before the first frame update
+    public void PlayDogFootstepSound()
+    {
+        AkSoundEngine.PostEvent("dog_foot", gameObject);
+        WwiseEvent.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
